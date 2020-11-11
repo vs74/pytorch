55b95308d8ab0bc19ee3ca3978cca5d5fbbde13e
Ashkan Aliabadi <ashkanaliabadi@fb.com>
2020-11-10 20:28:26 -0800

    Tweak Vulkan memory use.
    ghstack-source-id: c16eeee9c4a479e74a7d8ba52b105915b609f11c
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/47728

--- aten/src/ATen/native/vulkan/api/Resource.h
+++ aten/src/ATen/native/vulkan/api/Resource.h
@@ -12,7 +12,6 @@ namespace native {
 namespace vulkan {
 namespace api {
 
-
 struct Resource final {
   class Pool;
 
@@ -334,17 +333,17 @@ class Resource::Memory::Scope final {
 
 template<typename, typename Pointer>
 inline Resource::Memory::Handle<Pointer> Resource::Memory::map() const & {
-  void* map(const Memory& memory);
+  void* map(const Memory& memory, Access::Flags);
 
   return Handle<Pointer>{
-    reinterpret_cast<Pointer>(map(*this)),
+    reinterpret_cast<Pointer>(map(*this, Access::Read)),
     Scope(allocator, allocation, Access::Read),
   };
 }
 
 template<typename, Resource::Memory::Access::Flags kAccess, typename Pointer>
 inline Resource::Memory::Handle<Pointer> Resource::Memory::map() & {
-  void* map(const Memory& memory);
+  void* map(const Memory& memory, Access::Flags);
 
   static_assert(
       (kAccess == Access::Read) ||
@@ -353,7 +352,7 @@ inline Resource::Memory::Handle<Pointer> Resource::Memory::map() & {
       "Invalid memory access!");
 
   return Handle<Pointer>{
-    reinterpret_cast<Pointer>(map(*this)),
+    reinterpret_cast<Pointer>(map(*this, kAccess)),
     Scope(allocator, allocation, kAccess),
   };
 }
